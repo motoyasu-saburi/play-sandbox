6df929316565be2b88dc7a6af1d7507012b090d8
motoyasu.saburi <motoyasu.saburi@bizreach.co.jp>
2018-03-13 23:05:54 +0900

    filterの修正

--- app/filters/Filter.scala
+++ app/filters/Filter.scala
@@ -1,11 +1,21 @@
 package filters
 
 import javax.inject._
+
+import play.api.http.HttpFilters
 import play.api.mvc._
+import play.filters.headers.SecurityHeadersFilter
+
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class ExampleFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
+class Filter @Inject()(
+  implicit ec: ExecutionContext,
+  securityHeaders: SecurityHeadersFilter
+) extends EssentialFilter with HttpFilters {
+
+  val filters = Seq(securityHeaders)
+
   override def apply(next: EssentialAction) = EssentialAction { request =>
     next(request).map { result =>
       result.withHeaders("X-ExampleFilter" -> "foo")
