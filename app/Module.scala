a9645a8bb4e57869dd83dac64e34b628654922d1
motoyasu.saburi <motoyasu.saburi@bizreach.co.jp>
2018-03-13 23:22:34 +0900

    filterの有効化

--- app/Module.scala
+++ app/Module.scala
@@ -1,13 +1,10 @@
 import com.google.inject.AbstractModule
 import java.time.Clock
 
-import services.ApplicationTimer
-
 class Module extends AbstractModule {
 
   override def configure() = {
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
   }
 
 }
--- app/filters/Filter.scala
+++ app/filters/Filter.scala
@@ -1,24 +1,12 @@
 package filters
 
-import javax.inject._
+import javax.inject.Inject
 
 import play.api.http.HttpFilters
-import play.api.mvc._
 import play.filters.headers.SecurityHeadersFilter
 
-import scala.concurrent.ExecutionContext
-
-@Singleton
-class Filter @Inject()(
-  implicit ec: ExecutionContext,
+class Filter @Inject() (
   securityHeaders: SecurityHeadersFilter
-) extends EssentialFilter with HttpFilters {
-
+) extends HttpFilters {
   val filters = Seq(securityHeaders)
-
-  override def apply(next: EssentialAction) = EssentialAction { request =>
-    next(request).map { result =>
-      result.withHeaders("X-ExampleFilter" -> "foo")
-    }
-  }
 }
